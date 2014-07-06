9a1db32b4d2fa6d456dd851aa1f5a3073fcca42e
Alex Sexton <alexsexton@gmail.com>
2014-07-06 14:39:12 -0700

    Merge in code from the rework-function code. Don't quite know how it works yet.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ var colorguard = require('..');
 var fs = require('fs');
 
 describe('inspect', function() {
-  it('should detect rbg functions', function() {
+  it('should detect rgb functions', function() {
     var output = colorguard.inspect(fs.readFileSync(__dirname + '/fixtures/simple.css', 'utf8'));
     output.stats.counts['#000000'].should.equal(2);
   });
