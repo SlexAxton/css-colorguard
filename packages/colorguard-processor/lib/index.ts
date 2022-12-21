54e68ebe15ea9f38e83d49f75182640935c923ce
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2022-12-20 20:27:33 -0500

    :bug: fix: Fixed npm broken links

--- packages/colorguard-processor/lib/index.ts
+++ packages/colorguard-processor/lib/index.ts
@@ -1,9 +1,18 @@
-import postcss from "postcss";
-import plugin from "postcss-colorguard";
+import postcss from 'postcss';
+import plugin from 'postcss-colorguard';
 import formatter from './formatter';
 import reporter from 'postcss-reporter';
-import type { Options } from "../../../types/global";
+import type { Options } from '../../../types/global';
 
-export const processor = (opts: Options) => postcss([plugin(opts), reporter({ formatter: formatter, filter: function(message) { return true } })]);
+export const processor = (opts: Options) =>
+  postcss([
+    plugin(opts),
+    reporter({
+      formatter: formatter,
+      filter: function (message) {
+        return true;
+      },
+    }),
+  ]);
 
 export default plugin;
