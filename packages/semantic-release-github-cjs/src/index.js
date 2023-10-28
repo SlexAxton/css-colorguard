2dc129269a76dbca1d63835657df54d644c1f281
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-10-28 07:58:00 +0000

    :green_heart: ci: Fix typo

--- packages/semantic-release-github-cjs/src/index.js
+++ packages/semantic-release-github-cjs/src/index.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { verifyConditions, publish, addChannel, success, fail} = require('@semantic-release/github');
 
-module.exports = { verifyConditions, publish, addChannel success, fail };
+module.exports = { verifyConditions, publish, addChannel, success, fail };
