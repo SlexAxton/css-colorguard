563045e0c84bbb582c2b4793bea21d13a299aeea
Bryan Thomas <49354825+bryanjtc@users.noreply.github.com>
2023-10-28 07:42:13 +0000

    :green_heart: ci: Create cjs for github plugin

---
+++ packages/semantic-release-github-cjs/src/index.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { verifyConditions, publish, success, fail, addChannel } = require('@semantic-release/github');
+
+module.exports = { verifyConditions, publish, success, fail, addChanel };
